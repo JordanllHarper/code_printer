cbcad33cf261d7a70e2d467a92925c4708b96fa1
Jordan <46637683+JordanllHarper@users.noreply.github.com>
2023-05-18 00:36:30 +0100

    Extract creating paragraph to separate function

--- src/path_handling.rs
+++ src/path_handling.rs
@@ -1,42 +1,46 @@
-pub mod path_handling_mod{
-    use std::fs::{DirEntry, ReadDir};
-    use std::io::Error;
-    use crate::logger::logger_mod::{log, log_found_include, log_scan};
+pub mod path_handling_mod {
+    use crate::logger::logger_mod::{log_found_include, log_scan};
     use crate::node_result::node_result::NodeResult;
     use crate::read_directory::read_directory_mod::read_from_dir;
     use crate::read_file::read_file_mod::handle_non_dir;
+    use std::fs::{DirEntry, ReadDir};
+    use std::io::Error;
 
-    fn handle_each_path(dir_entry: Result<DirEntry, Error>, desired_file_extension: &str, file_include_sig: &str) -> Option<NodeResult> {
+    fn handle_each_path(
+        dir_entry: Result<DirEntry, Error>,
+        desired_file_extension: &str,
+        file_include_sig: &str,
+    ) -> Option<NodeResult> {
         let mut each_path_node_collector = NodeResult::new();
         let path = match dir_entry {
             Ok(p) => p,
             Err(_) => return None,
         }
-            .path();
+        .path();
 
         log_scan(&path.to_str().unwrap_or_default());
 
-
         if path.is_dir() {
-            let path_str = match path
-                .to_str() {
+            let path_str = match path.to_str() {
                 Some(c) => c,
                 None => return None,
             };
 
             //add all the contents of the child to this node result
             //the recursive call back to read_from_dir
-            let read_dir_result = &match read_from_dir(path_str, desired_file_extension, file_include_sig) {
-                Ok(c) => c,
-                Err(_) => NodeResult::new(),
-            };
+            let read_dir_result =
+                &match read_from_dir(path_str, desired_file_extension, file_include_sig) {
+                    Ok(c) => c,
+                    Err(_) => NodeResult::new(),
+                };
 
             if read_dir_result.include_content {
                 each_path_node_collector.include_content = true;
                 each_path_node_collector.contents += &read_dir_result.contents;
             }
         } else {
-            let delimit: Vec<&str> = path.file_name()
+            let delimit: Vec<&str> = path
+                .file_name()
                 .unwrap_or_default()
                 .to_str()
                 .unwrap_or_default()
@@ -55,9 +59,8 @@ pub mod path_handling_mod{
 
             let file_content = match handle_non_dir(path, delimit, desired_file_extension) {
                 Some(c) => c,
-                None => "".to_string()
+                None => "".to_string(),
             };
-
 
             each_path_node_collector.contents += &file_content;
         }
@@ -73,14 +76,11 @@ pub mod path_handling_mod{
         let mut collector_result = NodeResult::new();
 
         for path in paths {
-            let node_result = match handle_each_path(
-                path,
-                desired_file_extension,
-                file_include_sig) {
+            let node_result = match handle_each_path(path, desired_file_extension, file_include_sig)
+            {
                 Some(c) => c,
                 None => continue,
             };
-
 
             collector_result.contents += &node_result.contents;
             if node_result.include_content {
--- src/write_to_word_doc.rs
+++ src/write_to_word_doc.rs
@@ -5,16 +5,38 @@ pub mod write_to_word_doc_mod {
     pub fn write_to_word_doc(path: &str, contents: &str) -> String {
         let _path = std::path::Path::new(path);
         let file = std::fs::File::create(_path).unwrap();
-        let para = Paragraph::new().add_run(
-            Run::new()
-                .add_text(contents)
-                .fonts(RunFonts::new().ascii("Courier New")),
-        );
+        let para = create_paragraph(contents);
         let result = Docx::new().add_paragraph(para).build().pack(file);
 
         return match result {
             Ok(_) => "Successful write".to_string(),
             Err(_) => "Not successful write".to_string(),
         };
+    }
+
+    pub fn create_paragraph(contents: &str) -> Paragraph {
+        let para = Paragraph::new();
+
+        let para = para.add_run(
+            Run::new()
+                .add_text(contents)
+                .fonts(RunFonts::new().ascii("Courier New")),
+        );
+
+        return para;
+    }
+
+    #[test]
+    fn create_paragraph_test() {
+        let data = "print(\"Hello world\")";
+
+        let expected = Paragraph::new().add_run(
+            Run::new()
+                .add_text(data)
+                .fonts(RunFonts::new().ascii("Courier New")),
+        );
+
+        let actual = create_paragraph(data);
+        assert_eq!(expected.children().first(), actual.children().first());
     }
 }
